5e18fd0fdaaf12647db3e2920016d2bd24e9bfc7
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2025-07-24 12:19:49 +0300

    1

---
+++ js/mobile-menu.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const navListRef = document.querySelector(".y-w-navigation-list");
+  const headerRef = document.querySelector(".y-w-header");
+  const navigationRef = document.querySelector(".y-w-navigation");
+
+  menuBtnRef.addEventListener("click", () => {
+    menuBtnRef.classList.toggle("is-open");
+    navListRef.classList.toggle("is-open");
+    headerRef.classList.toggle("is-open");
+    navigationRef.classList.toggle("is-open");
+  });
+
+  document.querySelectorAll(".y-w-navigation-link").forEach((n) =>
+    n.addEventListener("click", () => {
+      navListRef.classList.remove("is-open");
+      menuBtnRef.classList.remove("is-open");
+      headerRef.classList.remove("is-open");
+      navigationRef.classList.remove("is-open");
+    })
+  );
+});
+
+function toggleAnswer(element) {
+  const answer = element.nextElementSibling;
+  const buttonImg = element.querySelector(".y-w-toggle-button img");
+
+  answer.classList.toggle("open");
+
+  if (answer.classList.contains("open")) {
+    buttonImg.src = "/img/Minus.png";
+    buttonImg.classList.add("rotated");
+  } else {
+    buttonImg.src = "/img/Add.png";
+    buttonImg.classList.remove("rotated");
+  }
+}
+document.addEventListener("DOMContentLoaded", function () {
+  const popup = document.getElementById("cookie-popup");
+  const acceptButton = document.getElementById("accept-cookies");
+  const declineButton = document.getElementById("decline-cookies");
+
+  if (!localStorage.getItem("cookiesAccepted")) {
+    popup.style.display = "block";
+  }
+
+  acceptButton.onclick = function () {
+    localStorage.setItem("cookiesAccepted", "true");
+    popup.style.display = "none";
+  };
+
+  declineButton.onclick = function () {
+    localStorage.setItem("cookiesAccepted", "false");
+    popup.style.display = "none";
+  };
+});
---
+++ js/slick.js
@@ -0,0 +1,46 @@
+$(function () {
+  $(".y-w-gallery-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+
+    cssEase: "linear",
+    prevArrow: ".y-w-gallery-list-left",
+    nextArrow: ".y-w-gallery-list-right",
+  });
+});
+
+$(function () {
+  $(".y-w-characters-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+
+    cssEase: "linear",
+    prevArrow: ".y-w-characters-list-left",
+    nextArrow: ".y-w-characters-list-right",
+  });
+});
+$(function () {
+  $(".y-w-reviews-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+
+    cssEase: "linear",
+    prevArrow: ".y-w-reviews-list-left",
+    nextArrow: ".y-w-reviews-list-right",
+  });
+});
